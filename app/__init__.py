ccd10081a38f27e67f5a67120ec88f363b4c0d87
Emmanuel <ceaselessflows@yahoo.com>
2026-07-23 17:56:39 +0100

    What is render using?

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,15 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from config import config
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.login_message = 'Please log in to access this page.'
-login_manager.login_message_category = 'info'
-
-
 def create_app(config_name=None):
     if config_name is None:
         config_name = os.environ.get('FLASK_ENV', 'default')
 
     app = Flask(__name__, template_folder='../templates')
     app.config.from_object(config[config_name])
+
+    # ADD THESE LINES
+    print("=" * 60)
+    print("FLASK_ENV:", config_name)
+    print("DATABASE URI:", app.config["SQLALCHEMY_DATABASE_URI"])
+    print("=" * 60)
 
     os.makedirs(app.config['DOWNLOAD_FOLDER'], exist_ok=True)
 
