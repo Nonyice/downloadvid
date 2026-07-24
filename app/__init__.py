7d7b2abb225dbd92b4c91fda123965eab0da08d6
Emmanuel <ceaselessflows@yahoo.com>
2026-07-24 18:46:52 +0100

    csfr

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 from config import config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+csrf = CSRFProtect()
+
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
@@ -18,21 +21,22 @@ def create_app(config_name=None):
     app = Flask(__name__, template_folder='../templates')
     app.config.from_object(config[config_name])
 
+    # DEBUG
+    print("=" * 60)
+    print("FLASK_ENV:", config_name)
+    print("DATABASE_URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
+    print("=" * 60)
+
     os.makedirs(app.config['DOWNLOAD_FOLDER'], exist_ok=True)
 
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     from app.auth.routes import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from app.main.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    # ADD THESE LINES HERE
-    from app.models import User, Download
-
-    with app.app_context():
-        db.create_all()
 
     return app
